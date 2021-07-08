4b57810661edbc6f84288fdb835ee9e7ed65cdff
Martial <martial.mailloux@gmail.com>
2021-07-08 15:22:02 +0200

    Cote + Clef

--- scripts/DriverOdriveROS.py
+++ scripts/DriverOdriveROS.py
@@ -13,11 +13,13 @@ class Robot_properties:
 		self.start = False
 		self.stop_timer = False
 		self.Initialisation = False
+		self.cote = 0		
 		self.AduinoOrder = 0
 
 		rospy.Subscriber("StateTirette", Bool, self.UpdateStart)
 		rospy.Subscriber("stop_timer", Bool, self.UpdateStop)
 		rospy.Subscriber("StateClef", Bool, self.UpdateKey)
+		rospy.Subscriber("StateCote", Bool, self.UpdateCote)
 
 		self.pub_arduino = '/arduinoOrder'
 		self.pub_arduino_topic = rospy.Publisher(self.pub_arduino, Int16, queue_size=1)
@@ -30,6 +32,10 @@ class Robot_properties:
 
 	def UpdateKey(self, data):
 		self.Initialisation = data.data
+
+	def UpdateCote(self, data):
+		self.cote = data.data
+
 
 	def Publish_ArduinoOrder(self,order):
 		arduino_command = Int16()
@@ -44,8 +50,9 @@ def main():
 
 	rospy.init_node('DriverOdrive', anonymous=True)
 	robot = Robot_properties()
-
-	while(not(robot.Initialisation)):
+	
+	"""Mode match = standby"""
+	while not(robot.Initialisation): 
 		sleep(0.1)
 
 	odrv0 = DriverOdrive.odrive.find_any()
@@ -66,40 +73,52 @@ def main():
 			Moteurs.Translation(dist)
 		else:
 			sleep(0.2)
-
-	print("========== MARCHE ARRIERE =========")
-	initial_dist = -1100
-	while(initial_dist - Moteurs.Rounds_To_Length(Moteurs.motor1.encoder.pos_estimate) < -10):
-		if(not(Moteurs.check_need_to_break())):
-			dist = initial_dist - Moteurs.Rounds_To_Length(Moteurs.motor1.encoder.pos_estimate)
-			print(dist)
-			Moteurs.Translation(dist)
-		else:
-			sleep(0.2)
-
-	print("========== ROTATION =========")
-	initial_angle = -90
-	Moteurs.Rotation(initial_angle)
-
-
-	print("========== SORTIR LA CREMAILLERE =========")
-	robot.Publish_ArduinoOrder(9)
-	sleep(2)	
 	
-	print("========== MARCHE ARRIERE =========")
-	initial_dist = -1500
-	while(initial_dist - Moteurs.Rounds_To_Length(Moteurs.motor1.encoder.pos_estimate) < -10):
-		if(not(Moteurs.check_need_to_break())):
-			dist = initial_dist - Moteurs.Rounds_To_Length(Moteurs.motor1.encoder.pos_estimate)
-			print(dist)
-			Moteurs.Translation(dist)
-		else:
-			sleep(0.2)
-	
-	print("========== RENTRER LA CREMAILLERE =========")
-	robot.Publish_ArduinoOrder(10)
-	sleep(2)
+	if not(robot.cote):
+
+		print("========== MARCHE ARRIERE =========")
+		initial_dist = -1100
+		while(initial_dist - Moteurs.Rounds_To_Length(Moteurs.motor1.encoder.pos_estimate) < -10):
+			if(not(Moteurs.check_need_to_break())):
+				dist = initial_dist - Moteurs.Rounds_To_Length(Moteurs.motor1.encoder.pos_estimate)
+				print(dist)
+				Moteurs.Translation(dist)
+			else:
+				sleep(0.2)
+
+		print("========== ROTATION =========")
+		initial_angle = -90
+		Moteurs.Rotation(initial_angle)
+
+		print("========== MARCHE AVANT =========")
+		initial_dist = 100
+		while(initial_dist - Moteurs.Rounds_To_Length(Moteurs.motor1.encoder.pos_estimate) > 10):
+			if(not(Moteurs.check_need_to_break())):
+				dist = initial_dist - Moteurs.Rounds_To_Length(Moteurs.motor1.encoder.pos_estimate)
+				print(dist)
+				Moteurs.Translation(dist)
+			else:
+				sleep(0.2)
+
+
+		print("========== SORTIR LA CREMAILLERE =========")
+		robot.Publish_ArduinoOrder(9)
+		sleep(2)	
 		
+		print("========== MARCHE ARRIERE =========")
+		initial_dist = -1500
+		while(initial_dist - Moteurs.Rounds_To_Length(Moteurs.motor1.encoder.pos_estimate) < -10):
+			if(not(Moteurs.check_need_to_break())):
+				dist = initial_dist - Moteurs.Rounds_To_Length(Moteurs.motor1.encoder.pos_estimate)
+				print(dist)
+				Moteurs.Translation(dist)
+			else:
+				sleep(0.2)
+		
+		print("========== RENTRER LA CREMAILLERE =========")
+		robot.Publish_ArduinoOrder(10)
+		sleep(2)
+			
 
 	print("========== Fin de homologation 2021 =========")
 
